dfbe96fac68af7b4be32ba88e09f56741c40722b
Rian Putra Mantovani <mantovanirian@gmail.com>
2017-01-14 13:17:16 +0700

    Day 2 Lear Python with the Hard Way (14-12017)

---
+++ exercise/ex10.py
@@ -0,0 +1,7 @@
+tabby_cat = "\t I am tabbed in"
+persian_cat = "I am split\non a line"
+backslash_cat = "I am \\ a \\ cat"
+
+print tabby_cat
+print persian_cat
+print backslash_cat
---
+++ exercise/ex11.py
@@ -0,0 +1,14 @@
+print "How Old Are You?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print  "how fat are you",
+fat = raw_input()
+print "add a + b"
+print "a= "
+a=int(raw_input())
+print "b= "
+b=int(raw_input())
+
+print "I am %r years with %r tall, %r and you can't teach that" %(age,height,fat)
+print "a+b= %r" %(a+b)
